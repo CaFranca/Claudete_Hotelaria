0ae5616f5aba8315617d5e33c0e4e2777d0905a6
Caíque <135059792+CaFranca@users.noreply.github.com>
2024-12-11 21:39:55 -0300

    Deu BOM

--- Projeto_Hotelaria/js/loginbucha.js
+++ Projeto_Hotelaria/js/loginbucha.js
@@ -1,8 +1,26 @@
-function Login(event) {
+//const senhaCriptografada = CryptoJS.SHA256(senha).toString(CryptoJS.enc.Base64);
+
+async function criptografar(message) {
+    // Codifica a mensagem para um ArrayBuffer
+    const msgBuffer = new TextEncoder().encode(message);
+  
+    // Cria um objeto de hash SHA-256
+    const hash = await window.crypto.subtle.digest('SHA-256', msgBuffer);
+  
+    // Converte o resultado do hash em uma string hexadecimal
+    const hashArray = Array.from(new Uint8Array(hash));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  
+    return hashHex;
+  }
+
+async function login(event) {
     event.preventDefault() // impede que o formulário seja enviado assim que o usuário aperte o botão (isso fazia com que a validação não ocorresse)
     var usuario = document.getElementsByName('username')[0].value.toLowerCase();
-    var senha = document.getElementsByName('password')[0].value.toLowerCase();
-    if (usuario == "admin" && senha == "admin") {
+    var senha = await criptografar(document.getElementsByName('password')[0].value);  
+    console.log(`Essa é a senha: ${senha}`)
+    if (usuario == "admin" && ( senha == await criptografar("admin"))) {
+        console.log("True")
         localStorage.setItem('logado', 'true');
         window.location.href = 'index.html';
     } else {
